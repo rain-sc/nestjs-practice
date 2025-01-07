90c993ea85ebf528b611c2ad76c05bcfab45bca1
Rain <shunchitrain@gmail.com>
2025-01-07 16:12:01 +0800

    download image

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,12 +1,16 @@
 import {
   Controller,
   Post,
+  Get,
   UseInterceptors,
   UploadedFile,
+  Res,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-
+import { join } from 'path';
+import { Response } from 'express';
+import { zip } from 'compressing';
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
@@ -17,5 +21,25 @@ export class UploadController {
     console.log('file', file);
 
     return '上傳圖片';
+  }
+
+  @Get('export')
+  downLoad(@Res() res: Response) {
+    const url = join(__dirname, '../images/1736236037739.png');
+    res.download(url);
+    return '下載圖片';
+  }
+
+  @Get('stream')
+  async down(@Res() res: Response) {
+    const url = join(__dirname, '../images/1736236037739.png');
+
+    const tar = new zip.Stream();
+    await tar.addEntry(url);
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', 'attachment; filename=example.zip');
+    tar.pipe(res);
+
+    return '下載圖片';
   }
 }
